c91a89bff281b57413dc868a97345ea8d9fcaa08
ardlema <ardlema@gmail.com>
2015-03-14 00:59:39 +0100

    Trying to make the plugins work

---
+++ project/ApplicationBuild.scala
@@ -0,0 +1,77 @@
+import sbt.Keys._
+import sbt._
+
+object ApplicationBuild extends Build {
+
+  Resolver.sonatypeRepo("releases")
+
+  import org.scalastyle.sbt.ScalastylePlugin.{Settings => scalastyleSettings}
+  import scoverage.ScoverageSbtPlugin.{buildSettings => scoverageSettings}
+
+  object Versions {
+    val spark = "1.2.1"
+  }
+
+  val customScalacOptions = Seq(
+    "-deprecation",
+    "-encoding", "UTF-8",
+    "-feature",
+    "-language:existentials",
+    "-language:higherKinds",
+    "-language:implicitConversions",
+    "-unchecked",
+    "-Xfatal-warnings",
+    "-Xfuture",
+    "-Xlint",
+    "-Yno-adapted-args",
+    "-Ywarn-dead-code",
+    "-Ywarn-numeric-widen",
+    "-Ywarn-unused-import")
+
+  val customJavaInRuntimeOptions = Seq(
+    "-Xmx512m"
+  )
+
+  val customJavaInTestOptions = Seq(
+    "-Xmx512m"
+  )
+
+  val customResolvers = Seq(
+    Classpaths.sbtPluginReleases,
+    Classpaths.typesafeReleases,
+    "Sonatype OSS Releases" at "http://oss.sonatype.org/content/repositories/releases/",
+    "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases"
+  )
+
+  val customLibraryDependencies = Seq(
+    "org.apache.spark" %% "spark-core" % Versions.spark,
+    "org.apache.spark" %% "spark-streaming" % Versions.spark,
+
+    "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0",
+
+    "org.slf4j" % "slf4j-api" % "1.7.10",
+    "ch.qos.logback" % "logback-classic" % "1.1.2",
+    "org.apache.kafka" % "kafka_2.10" % "0.8.2.0",
+
+    "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+  ).map(_.exclude(
+    "org.slf4j", "slf4j-log4j12"
+  ))
+
+  lazy val main = Project(
+    id = "big-brother",
+    base = file("."),
+    settings = Seq(
+      version := "1.0",
+      scalaVersion := "2.11.4",
+      javaOptions in Runtime ++= customJavaInRuntimeOptions,
+      javaOptions in Test ++= customJavaInTestOptions,
+      scalacOptions ++= customScalacOptions,
+      resolvers ++= customResolvers,
+      libraryDependencies ++= customLibraryDependencies,
+      parallelExecution in Test := false,
+      fork in Test := true
+    ) ++ scalastyleSettings ++ scoverageSettings
+  )
+
+}
