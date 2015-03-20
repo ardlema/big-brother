9716d46d1f6a337f928ae9ca5c132a1af588e912
Alberto Rodriguez <arodriguez@stratio.com>
2015-03-20 10:35:51 +0100

    Test refactoring

--- src/test/scala/org/ardlema/DirectKakfaWordCountTest.scala
+++ src/test/scala/org/ardlema/DirectKakfaWordCountTest.scala
@@ -1,24 +1,18 @@
 package org.ardlema
 
 import java.nio.file.Files
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.duration._
 
 import _root_.kafka.producer.KeyedMessage
-import org.apache.spark.rdd.RDD
 import kafka.serializer.StringDecoder
-import kafka.message.Message
-import org.apache.spark.streaming._
-import org.apache.spark.streaming.dstream.InputDStream
-import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka.KafkaUtils
 import org.kafka.KafkaProducer
 import org.scalatest._
 import org.scalatest.concurrent.Eventually
-import org.apache.spark.streaming._
-import org.apache.spark.SparkConf
-
-import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
-import scala.concurrent.duration._
 
 class DirectKakfaWordCountTest
   extends FlatSpec
@@ -28,56 +22,42 @@ class DirectKakfaWordCountTest
   with BeforeAndAfter
   with KafkaProducer {
 
+  val appName = "kafka-word-count-app"
   val windowDuration = Seconds(4)
   val slideDuration = Seconds(2)
-  val master = "local[2]"
-  val appName = "example-spark-streaming-direct-kafka"
   val batchDuration = Seconds(1)
+  val sparkContext = createSparkContext
+  val sparkStreamingContext = createSparkStreamingContext
   val checkpointDir = Files.createTempDirectory(appName).toString
-  val conf = new SparkConf()
-    .setMaster(master)
-    .setAppName(appName)
-    .set("spark.streaming.clock", "org.apache.spark.streaming.util.ManualClock")
-  val sc = new SparkContext(conf)
-  val ssc = new StreamingContext(sc, batchDuration)
-  ssc.checkpoint(checkpointDir)
-  val kafkaParams = Map[String, String]("metadata.broker.list" -> "localhost:9092")
-
-  after {
-    if (sc != null) sc.stop()
-    if (ssc != null) ssc.stop(stopSparkContext = false, stopGracefully = false)
-  }
+  sparkStreamingContext.checkpoint(checkpointDir)
 
   behavior of "WordCounter"
-
-  "Sample set" should "be counted" in {
+  "Sample set of messages" should "be counted" in {
     withKafkaProducer { kafkaProducer =>
-      val clock = new ClockWrapper(ssc)
-
+      val clock = new ClockWrapper(sparkStreamingContext)
       val topic = "testTopic"
+      //We need to put a dummy message in the topic to avoid the "Couldn't find leader offsets for Set()" error
       val dummyMessage = new KeyedMessage[String, String](topic, "dummyMessage")
       kafkaProducer.send(dummyMessage)
 
       Given("streaming context is initialized and we have at least one message in the kafka topic")
       val topics = Set(topic)
-      val messages: InputDStream[(String, String)] = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
-        ssc, kafkaParams, topics)
-
-      //TODO: Try to get rid of this mutable variable
+      val kafkaParams = Map[String, String]("metadata.broker.list" -> "localhost:9092")
+      val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+        sparkStreamingContext, kafkaParams, topics)
+      //TODO: Try to get rid of this mutable variable!!
       var results = ListBuffer.empty[Array[DirectKakfaWordCount]]
-
-      DirectKakfaWordCount.countKafkaMessages(messages, windowDuration, slideDuration) { (wordsCount: RDD[DirectKakfaWordCount], time: Time) =>
-        results += wordsCount.collect()
+      DirectKakfaWordCount.countKafkaMessages(messages, windowDuration, slideDuration) {
+        (wordsCount: RDD[DirectKakfaWordCount], time: Time) =>
+          results += wordsCount.collect()
       }
+      sparkStreamingContext.start()
 
-      ssc.start()
-
-      When("first set of words queued")
+      When("first set of words sent to the broker")
       val messageAmparo = new KeyedMessage[String, String](topic, "amparo")
       val messageManuela = new KeyedMessage[String, String](topic, "manuela")
       kafkaProducer.send(messageAmparo)
       kafkaProducer.send(messageManuela)
-
 
       Then("words counted after first slide")
       clock.advance(slideDuration.milliseconds)
@@ -88,7 +68,7 @@ class DirectKakfaWordCountTest
             DirectKakfaWordCount("manuela", 1)))
       }
 
-      When("second set of words queued")
+      When("second set of words sent to the broker")
       val messageAntonia = new KeyedMessage[String, String](topic, "antonia")
       kafkaProducer.send(messageManuela)
       kafkaProducer.send(messageAntonia)
@@ -103,7 +83,7 @@ class DirectKakfaWordCountTest
             DirectKakfaWordCount("antonia", 1)))
       }
 
-      When("nothing more queued")
+      When("nothing more sent to the broker")
 
       Then("word counted after third slide")
       clock.advance(slideDuration.milliseconds)
@@ -115,7 +95,7 @@ class DirectKakfaWordCountTest
             DirectKakfaWordCount("antonia", 1)))
       }
 
-      When("nothing more queued")
+      When("nothing more sent to the broker")
 
       Then("word counted after fourth slide")
       clock.advance(slideDuration.milliseconds)
@@ -127,5 +107,21 @@ class DirectKakfaWordCountTest
             DirectKakfaWordCount("antonia", 0)))
       }
     }
+  }
+
+  def createSparkContext = {
+    val master = "local[2]"
+    val conf = new SparkConf()
+      .setMaster(master)
+      .setAppName(appName)
+      .set("spark.streaming.clock", "org.apache.spark.streaming.util.ManualClock")
+    new SparkContext(conf)
+  }
+
+  def createSparkStreamingContext = new StreamingContext(sparkContext, batchDuration)
+
+  after {
+    if (sparkContext != null) sparkContext.stop()
+    if (sparkStreamingContext != null) sparkStreamingContext.stop(stopSparkContext = false, stopGracefully = false)
   }
 }
