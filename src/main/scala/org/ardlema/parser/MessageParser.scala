2fdfcc7484d4ab0141ff121a3cfacff7cae3b097
Alberto Rodriguez <arodriguez@stratio.com>
2015-03-26 17:13:04 +0100

    Fixing scalastyle warns

--- src/main/scala/org/ardlema/parser/MessageParser.scala
+++ src/main/scala/org/ardlema/parser/MessageParser.scala
@@ -1,3 +1,18 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package org.ardlema.parser
 
 import com.vividsolutions.jts.geom.Geometry
@@ -12,7 +27,7 @@ case class ParseError(errorMessage: ParserErrorMessages)
 object GeomUtils {
   val SRID = 4326
 
-  def parseGeometry(geomWkt: String) = {
+  def parseGeometry(geomWkt: String): Try[Geometry] = {
     val reader = new WKTReader
     Try {
       val geom = reader.read(geomWkt)
@@ -28,10 +43,12 @@ object MessageParser {
 
   def parse(message: String): \/[ParseError, Message] = {
     val fields = message.split(separator)
-    if (emptyFields(fields)) -\/(ParseError(EmptyMessage))
-    else parseFields(fields) match {
-      case Success(message) => \/-(message)
-      case Failure(ex) => -\/(ParseError(WrongFieldFormatMessage))
+    if (emptyFields(fields)) { -\/(ParseError(EmptyMessage)) }
+    else {
+      parseFields(fields) match {
+        case Success(message) => \/-(message)
+        case Failure(ex) => -\/(ParseError(WrongFieldFormatMessage))
+      }
     }
   }
 
--- src/main/scala/org/ardlema/spark/DwellDetector.scala
+++ src/main/scala/org/ardlema/spark/DwellDetector.scala
@@ -1,3 +1,18 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package org.ardlema.spark
 
 import com.vividsolutions.jts.geom.Geometry
@@ -22,15 +37,13 @@ class DwellDetector {
     val geometriesPerUser = events
       .filter(input => properMessage(input._2))
       .map(e => {
-          val message = MessageParser.parse(e._2).toOption.get
-          (message.userId, message.geometry)
-        }).groupByKeyAndWindow(windowDuration, slideDuration)
-
+      val message = MessageParser.parse(e._2).toOption.get
+      (message.userId, message.geometry)
+    }).groupByKeyAndWindow(windowDuration, slideDuration)
+  }
     //TODO: Find out geometries that intersect to each other
     /*dwellDetected.foreachRDD((rdd: RDD[Dwell], time: Time) => {
       handler(rdd.sortBy(_.userId), time)
     })*/
-  }
-  
   def properMessage(message: String): Boolean = if (MessageParser.parse(message).isRight) true else false
 }
